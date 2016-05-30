8d59fd35ad171606b187d1fa9cc70630d11a2dae
Merloni <tuomo.salmenkivi@gmail.com>
2016-05-30 20:27:56 +0300

    added testdata to dbtables

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,8 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+
+INSERT INTO Apartment (name, password) VALUES ('A1', 'Kalle123'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
+INSERT INTO Apartment (name, password) VALUES ('A2', 'Henri123');
+-- Game taulun testidata
+INSERT INTO Reservation (reserved, reserved) VALUES (true, NOW());
+
